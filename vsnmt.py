84451e887a8d07f658902c10b4bba4eb8d4303c9
tamires <tami4lves00@gmail.com>
2026-09-16 14:50:07 -0300

    opções de escolha

--- vsnmt.py
+++ vsnmt.py
@@ -2,10 +2,34 @@ alunos = []
 
 def adicionar_aluno():
     nome = input("Digite o nome do aluno: ")
-    alunos.append(nome)
+    alunos.append(nome.capitalize())
     print("Aluno cadastrado com sucesso!")
 
 def listar_alunos():
     alunos.sort()
     for indice, aluno in enumerate(alunos, start = 1):
         print(indice, ": " + aluno)
+
+while True:
+
+    print("\n1 - Adicionar aluno")
+    print("2 - Listar alunos")
+    #print("3 - Excluir alunos")
+    print("4 - Sair")
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        adicionar_aluno()
+
+
+
+    elif opcao == "2":
+        listar_alunos()
+
+    #elif opcao == "3":
+        #exclusao()
+    elif opcao == "4":
+        break
+
+    else:
+        print("Opção inválida!")
